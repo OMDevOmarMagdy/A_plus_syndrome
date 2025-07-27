dd767672e144147595d275c860a55063f00e7d65
Omar Magdy <om8622145@gmail.com>
2025-07-28 02:55:03 +0300

    try

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const serverless = require("serverless-http");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerOptions = require("./utils/swaggerOptions");
+
+const authRouter = require("./Routes/authRoutes");
+
+dotenv.config();
+
+// ============= Create the App =============
+const app = express();
+
+// ============= Cors =============
+const cors = require("cors");
+app.use(cors());
+
+// ============= Middlewares =============
+app.use(express.json());
+
+// ============= Swagger =============
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+// ============= Database Connection =============
+const db = process.env.DB_CONNECTION;
+mongoose.connect(db).then(() => {
+  console.log("DB Connect Successfully");
+});
+
+// ============= Routes =============
+app.use("/api/v1/auth", authRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Global error middleware
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    status: err.status,
+    message: err.message || "Something went wrong",
+  });
+});
+
+// Export for Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
